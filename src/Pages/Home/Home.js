92d26320f43976930bfa4dc106356c33878428bd
Ilza <ilzammed2@gmail.com>
2020-07-23 18:07:11 -0300

    add gifs and remove buttons

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import MainTitle from '../../Components/MainTitle/MainTitle'
-// import Button from '../../Components/Button/Button';
 import GeneralTemplate from '../../Template/GeneralTemplate/GeneralTemplate';
-import TextComponent from '../../Components/TextComponent/TextComponent';
 
 import './Home.css';
 
@@ -15,17 +13,16 @@ class Home extends Component {
                     <MainTitle
                         mainTitle='Que Documentação?'
                     />
-                    <TextComponent
-                        textContent='Se você já tentou trabalhar com uma nova linguagem, usar o projeto de outros desenvolvedores ou usar um framework sem olhar a documentação, sabe o quanto é difícil. A documentação nos ajuda a entender quais os objetivos, como usar e como manter as aplicações. Aqui você encontra dicas de como pesquisar, como fazer um README.md e algumas das principais documentações!'
-                    />
-                    {/* <Button
-                        path='about'
-                        btnText='Sobre'
-                    />
-                    <Button
-                        path='docs'
-                        btnText='Documentação'
-                    /> */}
+                    <div className='textContainer'>
+                        <p >Você já tentou trabalhar com uma nova linguagem, usar o projeto de outros desenvolvedores ou usar um framework sem olhar a documentação?</p>
+                        <img src='https://media.giphy.com/media/3ornk6UHtk276vLtkY/giphy.gif' alt='gif de luke skywalker dizendo que é impossível'></img>
+                        <p>É difícil né? </p>
+                        <p >
+                            Cada linguagem tem a sua, algumas mais claras do que outras, outras ainda com exemplo e tem linguagens que carecem de ter essa ferramenta importantíssima para as(os) devs.
+    
+                           A documentação nos ajuda a entender quais os objetivos, como usar e como manter as aplicações. Aqui você encontra dicas de como pesquisar e algumas das principais documentações!</p>
+                    </div>
+
                 </div>
             </GeneralTemplate>
         );
