37a2884a882b6572dc1e1bf4b61aef72a7d2cf7a
Ilza <ilzammed2@gmail.com>
2020-07-23 13:52:05 -0300

    CHANGE general template

--- src/Template/GeneralTemplate/GeneralTemplate.js
+++ src/Template/GeneralTemplate/GeneralTemplate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import './GeneralTemplate.css';
 
@@ -7,20 +9,22 @@ const GeneralTemplate = ({ children }) => {
     return (
         <div className='generalTemplate__container'>
             <input type="checkbox" id="menu"></input>
-            <label for="menu" class="menu">Menu</label>
+            <label for="menu" class="menu">
+            <FontAwesomeIcon icon={faBars} />
+                </label>
             <nav className=''>
                 <div>
                     <Link to='/'><p>Que documentação?</p></Link>
                     <ul className='generalTemplate__list'>
                         <Link to='/'><li>Home</li></Link>
-                        <Link to='/about'><li>Sobre</li></Link>
-                        <Link to='/docs'><li>Documentação</li></Link>
+                        <Link to='/about'><li>Dicas</li></Link>
+                        <Link to='/docs'><li>Documentações</li></Link>
                     </ul>
                 </div>
             </nav>
             {children}
         </div>
-
+// {["fas", "bars"]}
     )
 }
 
