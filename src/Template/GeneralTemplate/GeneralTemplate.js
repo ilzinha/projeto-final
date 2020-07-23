e54452d2ad155d07f088c0f1b0861dd685096f9b
Ilza <ilzammed2@gmail.com>
2020-07-23 18:06:06 -0300

    add footer

--- src/Template/GeneralTemplate/GeneralTemplate.js
+++ src/Template/GeneralTemplate/GeneralTemplate.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import './GeneralTemplate.css';
 
 const GeneralTemplate = ({ children }) => {
     return (
         <div className='generalTemplate__container'>
-            <input type="checkbox" id="menu"></input>
-            <label for="menu" class="menu">
-            <FontAwesomeIcon icon={faBars} />
-                </label>
+            <input type='checkbox' id='menu'></input>
+            <label for='menu' className='menu'>
+                <FontAwesomeIcon icon={faBars} />
+            </label>
             <nav className=''>
                 <div>
                     <Link to='/'><p>Que documentação?</p></Link>
@@ -22,9 +22,14 @@ const GeneralTemplate = ({ children }) => {
                     </ul>
                 </div>
             </nav>
+
             {children}
+
+            <footer className='footer'>
+                <p>2020 Projeto final do Bootcamp da reprograma por <a href='https://ilzinha.github.io/' target='_blank' rel="noopener noreferrer"><span>Ilza Medeiros</span></a></p>
+            </footer>
         </div>
-// {["fas", "bars"]}
+
     )
 }
 
