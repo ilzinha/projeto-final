e633c07fa645213c6944a0bb98cff5ac5a557e51
Ilza <ilzammed2@gmail.com>
2020-07-23 21:02:38 -0300

    ajusts on home content and gifs in page about

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
                         <img src='https://media.giphy.com/media/3ornk6UHtk276vLtkY/giphy.gif' alt='gif de luke skywalker dizendo que é impossível'></img>
                         <p>É difícil né? </p>
                         <p >
-                            Cada linguagem tem a sua, algumas mais claras do que outras, outras ainda com exemplo e tem linguagens que carecem de ter essa ferramenta importantíssima para as(os) devs.
+                            Cada linguagem tem a sua, algumas mais claras do que outras, outras possuem bons exemplos e tem linguagens que carecem de ter essa importante ferramenta para as(os) devs.
     
                            A documentação nos ajuda a entender quais os objetivos, como usar e como manter as aplicações. Aqui você encontra dicas de como pesquisar e algumas das principais documentações!</p>
                     </div>
